cfcb64cdb8d2f8177de46c79a3864f608e4c9ac1
kabowser <k.a.bowser32@gmail.com>
2022-11-13 17:06:43 -0800

    Finished assessment 3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,49 +11,123 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+  // fdescribe("take in number>2 give array", () => {
+  //   it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+  //     expect(takeIn2GiveFib(fibLength1)).toEqual([array])
+  //     expect(takeIn2GiveFib(fibLength2)).toEqual([array])
+  //   })
+  // })
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
+//const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
+//const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+//pseudocode
+//create a function called takeIn2GiveFib
+//the function will take in a number greater than 2 and give us an array of that numbers fibonacci sequence
+// fibonacci sequence is The sequence follows the rule that each number is equal to the sum of the preceding two numbers.
+//need to consider built in method. maybe our input number should be an array as well. Or will need to figure out how to make number data type into an array of numbers.
+//I think I will need to use some sort of iteration or for loop.
+//use to string method to convert number to string and then string to array.
+//input: number >3
+//output: [0,1,1,2,3]
+// const takeIn2GiveFib = () => {
+//   if(fibLength1 > 2) {
+//     return fibLength1[0] + fibLength1[1], i++
+//   } else if(fibLength2 >2) {
+//     return fibLength2[0] + fibLength2[1]
+//   }
+// }
+//ReferenceError: i is not defined
+// Originally I was trying to think of a way to convert a number data type into a string and then into an array. Maybe the return statements should look like something to contain return array[]
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
+// fdescribe("sorted from least to greatest", () => {
+//   it("takes in an object and returns an array of the values sorted from least to greatest", () => {
+//     expect(funcArrayStudy1(studyMinutesWeek1)).toEqual(arrayStudyWeek1)
+//     expect(funcArrayStudy2(studyMinutesWeek1)).toEqual(arrayStudyWeek2)
+//   })
+// })
+  //good error message
+// const studyMinutesWeek1 = {
+//   sunday: 90,
+//   monday: 30,
+//   tuesday: 20,
+//   wednesday: 15,
+//   thursday: 30,
+//   friday: 15,
+//   saturday: 60
+// }
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
+// const studyMinutesWeek2 = {
+//   sunday: 100,
+//   monday: 10,
+//   tuesday: 45,
+//   wednesday: 60,
+//   thursday: 20,
+//   friday: 15,
+//   saturday: 65
+// }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
+//pseudocode
+//function name is leastToGreatest
+//input: an object
+//output: an array of numbers from least to greatest.
+//studyMinutesWeek1 is a key with a value of {sunday: 90,
+  // monday: 30,
+  // tuesday: 20,
+  // wednesday: 15,
+  // thursday: 30,
+  // friday: 15,
+  // saturday: 60}
+//studyMinutesWeek2 is a key with a value of {
+//   sunday: 100,
+//   monday: 10,
+//   tuesday: 45,
+//   wednesday: 60,
+//   thursday: 20,
+//   friday: 15,
+//   saturday: 65
+// }
+// I will want to use map method to check every element if it is lower than the next number
+//return that number if it is lower and then iterate through the rest.
 
+// I think i need to try to destructure the object
+// I'm going to return to this problem
+// I forgot about the hint!
+//obkect.values will return an array of the values. I can then used a built in method like .sort to put them in order of smallest to greatest.
+
+// const arrayStudyWeek1 = Object.values(studyMinutesWeek1) 
+      
+
+// const funcArrayStudy1 = (arrayStudyWeek1) => {
+//   return arrayStudyWeek1.sort((a, b) => a - b)}
+
+// const arrayStudyWeek2 = Object.values(studyMinutesWeek2)
+// const funcArrayStudy2 = (arrayStudyWeek2) => {
+//   return arrayStudyWeek2.sort((a, b) => a - b)
+// }
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("array returns array of sum", () => {
+  it("takes in an array and returns an array of the accumulating sum.", () => {
+    expect(transactionSum(accountTransactions1)).toEqual()
+    expect(transactionSum(accountTransactions2)).toEqual()
+    expect(transactionSum(accountTransactions3)).toEqual()
+  })
+})
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -63,5 +137,14 @@ const accountTransactions2 = [250, -89, 100, -96]
 
 const accountTransactions3 = []
 // Expected output: []
-
+// Test passed
 // b) Create the function that makes the test pass.
+//pseudocode
+//function called transactionSum
+//input: accountTransactions1, accountTransactions1, accountTransactions3
+//output: array of number sumed together.
+// I think I can use .map that way the same length array is returned.
+// My brain is fried.
+    const transactionSum = () => {
+
+    }
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is a way for us to write html code in our app.js file while we using create react app. Whatever is in the JSX code block cant be accessed. If you are tring to access a certain javascript variable or function you have to put that variable in {}. The function invocation is a little different as well. The function will be wrapped in <function />
 
-Researched answer:
+Researched answer: JSX is a building block of code that allows users to interact. There a very specific and special jsx tags that are called react fragments.
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a hub of packaged code that contains code from other developers. More specifically it is a package manager. When ran in terminal yarn jest will add the necessary node modules in order to write tests to test our code. We all use yarn when creating react apps. Such as yarn create react-app file-name-student. Once the modules have been "installed"? we are clear to run yarn start which will show us a live local server to see in real time what is happening with our code.
 
-Researched answer:
+Researched answer: Allows us to add dependencies to our files/ projects. 
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: I have a flash card that I wrote during lecture. I believe an anonymous function is the bare bones of a function such as function functionName = () => {}
 
-Researched answer:
+Researched answer: Apparently I was wrong. I tried to check w3 school for their definition but was unsuccesful so I just googled it. Anonymous is anonymous because there is no function name! Can only be accessed by a variable it is stored in as a value.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: state is used for dynamic data. Props are a way of connection data like a pipeline from on component to another, like app.js
 
-Researched answer:
+Researched answer: State is variable that stores data that determines how the component renders and behaves. Components are comprised of data with state or without state. {useState} Props are specialized react object, that passes information from one component to another when using react creat-app. It is a flow of information. 
 
 5. What is the DOM?
 
-Your answer:
+Your answer: It is what provides the user interaction 
 
-Researched answer:
+Researched answer: DOM stands for Document Object Model. It can be described as a technique to provide visual representation of code. It is what the user sees when looking at a web browser.
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: <div></div> are html tags that are extremely helpful for organizing data and compartmentalizing that data to make it easier to identify sections of code.
 
-Researched answer:
+Researched answer: Span is a generic inline container element. Div is a contais html elements that is helpful to use when using CSS, allows easy identification of code block to be edited or coded with CSS properties.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: open source, object-oriented programming language deals large software systems
 
-2. Ruby:
+2. Ruby: open source, object-oriented programming language
 
-3. Implicit return:
+3. Implicit return: no return, value of the last line of a method is automatically returned.
 
-4. Ruby blocks:
+4. Ruby blocks: if key word or else starts the block, keyword end is used to specifiy the end scope of code.
 
-5. Ruby hashes:
+5. Ruby hashes: a dictionary container used in ruby that contains helpful and frequently used unique keys and corresponding values.
